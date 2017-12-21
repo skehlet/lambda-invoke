65294b09019e0b8528015fd32699d8bfa11b329c
Steve Kehlet <skehlet@nextgen.com>
2017-12-21 08:49:43 -0800

    working on changing what it does

--- lambda/index.js
+++ lambda/index.js
@@ -1,37 +1,57 @@
-const http = require('http');
+// const http = require('http');
+// const https = require('https');
+const axios = require('axios');
+const Promise = require('bluebird');
+const querystring = require('querystring');
 
 exports.handler = function(event, context, callback) {
-    // ...
-
     console.log('event:', JSON.stringify(event, null, 4));
 
+    let sourceIP = event.sourceIP;
+    let endpoint = 'http://freegeoip.net/json/' + encodeURIComponent(sourceIP);
 
-    let endpoint = 'http://freegeoip.net/json/';
+    axios.get(endpoint).then(function (response) {
+	    callback(null, JSON.stringify(response, null, 4));
+    }).catch(function (err) {
+		console.log("Error: " + err);
+	    callback(err);
+    });
 
-    let ip = event.ip;
-    if (ip) {
-    	endpoint += ip;
-    }
 
-	http.get(endpoint, (resp) => {
-	  let data = '';
 
-	  // A chunk of data has been recieved.
-	  resp.on('data', (chunk) => {
-	    data += chunk;
-	  });
+	// https.get(endpoint, (resp) => {
+	//   let data = '';
 
-	  // The whole response has been received. Print out the result.
-	  resp.on('end', () => {
-		console.log('end:', data);
-	//     console.log(JSON.parse(data).explanation);
-		callback(null, JSON.parse(data));
-	  });
+	//   // A chunk of data has been recieved.
+	//   resp.on('data', (chunk) => {
+	//     data += chunk;
+	//   });
 
-	}).on("error", (err) => {
-		console.log("Error: " + err.message);
-	    callback(err.message);
-	});
+	//   // The whole response has been received. Print out the result.
+	//   resp.on('end', () => {
+	// 	console.log('end:', data);
+	// //     console.log(JSON.parse(data).explanation);
+	// 	callback(null, JSON.parse(data));
+	//   });
+
+	// }).on("error", (err) => {
+	// 	console.log("Error: " + err.message);
+	//     callback(err.message);
+	// });
+
+
+
+
+
+
+    // let location = event.location;
+    // if (!location) {
+    // 	location = 'Irvine, California';
+    // }
+    // let params = {
+    // 	query: location
+    // };
+    // let endpoint = 'https://www.metaweather.com/api/location/search/?' + querystring.stringify(params);
 
 }
 
